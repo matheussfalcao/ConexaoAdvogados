5bb0da27510dbe31aa5ecb53667b77f481834a0d
Raquel Fonsec <raque-leto@hotmail.com>
2024-07-12 21:01:16 +0200

    Refatorar rotas e remover serviço de verificação da OAB
    - Ajustei as rotas para incluir CRUD completo de advogados ().
    - Removi o serviço de verificação da OAB, refletindo mudanças nos requisitos.

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,11 @@ Rails.application.routes.draw do
   get 'lawyers/verify_oab', to: 'lawyers#verify_oab'
 
   devise_for :users
+
   root to: "pages#home"
   get "sobre", to: "about#sobre"
   get "adv", to: "about#adv"
 
-  get "up", to: "rails/health#show", as: :rails_health_check
 
-  resources :lawyers do
-    collection do
-      get 'search'
-    end
-    resources :connections, only: [:new, :create]
-  end
-
-  resources :connections, except: [:new, :create] do
-    resources :payments, only: :new
-  end
-
-  resources :users, only: [:show, :new, :index, :create, :edit, :update, :destroy] do
-    collection do
-      get 'search'
-    end
-  end
+  resources :lawyers
 end

e3399a6dcf1650418ce5d0aae8a87c5426865608
Raquel Fonsec <raque-leto@hotmail.com>
2024-07-14 00:00:25 +0200

    Removed Devise confirmable migrations for users table

--- db/migrate/20240713172919_add_confirmable_to_devise.rb
+++ db/migrate/20240713172919_add_confirmable_to_devise.rb
@@ -1,11 +1,13 @@
-class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
+class AddDeviseConfirmableToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :confirmation_token, :string
-    add_column :users, :confirmed_at, :datetime
-    add_column :users, :confirmation_sent_at, :datetime
-     add_column :users, :unconfirmed_email, :string 
+    change_table :users do |t|
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
+    end
 
     add_index :users, :confirmation_token, unique: true
-    User.update_all confirmed_at: DateTime.now
   end
 end
--- db/migrate/20240713175726_add_devise_confirmable_to_users.rb
+++
@@ -1,4 +0,0 @@
-class AddDeviseConfirmableToUsers < ActiveRecord::Migration[7.1]
-  def change
-  end
-end

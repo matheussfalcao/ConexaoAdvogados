2c4ede3916428f2a3d4172c074cc0cb40eafafb0
Raquel Fonsec <raque-leto@hotmail.com>
2024-07-14 00:21:11 +0200

    Fix: Add Devise confirmable columns to Users table

--- db/migrate/20240713180759_add_confirmable_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddConfirmableToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :confirmation_token, :string
-  end
-end
--- db/migrate/20240713220615_remove_confirmable_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveConfirmableFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :confirmation_token, :string
-  end
-end

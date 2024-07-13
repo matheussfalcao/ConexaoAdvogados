3d17b3109246f4bc06587ae05933c9d3e8875cee
Raquel Fonsec <raque-leto@hotmail.com>
2024-07-13 23:49:39 +0200

    ajustes nas tabelas devise

--- db/migrate/20240713172919_add_confirmable_to_devise.rb
+++ db/migrate/20240713172919_add_confirmable_to_devise.rb
@@ -1,13 +1,11 @@
-class AddDeviseConfirmableToUsers < ActiveRecord::Migration[7.1]
+class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
   def change
-    change_table :users do |t|
-      ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
-    end
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+     add_column :users, :unconfirmed_email, :string 
 
     add_index :users, :confirmation_token, unique: true
+    User.update_all confirmed_at: DateTime.now
   end
 end

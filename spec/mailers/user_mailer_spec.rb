f6a40ae9a70a9405ba05b47bf3e553bdf1d5a74a
Raquel Fonsec <raque-leto@hotmail.com>
2024-07-14 02:45:43 +0200

    Modificar testes do mailer de usuário

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,44 +1,51 @@
+
 require 'rails_helper'
-
 RSpec.describe RegistrationsController, type: :controller do
-  let(:valid_params) {
-    {
-      user: {
-        name: 'Raquel',
-        lastname: 'Silva',
-        cpf: '12345678909',
-        oab: '123456',
-        sex: 'Female',
-        birth_date: '1990-01-01',
-        city: 'São Paulo',
-        state: 'SP',
-        phone: '11987654321',
-        email: 'test@example.com',
-        password: 'password123',
-        password_confirmation: 'password123',
-        terms_of_use: '1'
-      }
-    }
-  }
-
   before do
     @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
-  describe 'POST #create' do
-    context 'with valid params' do
-      it 'creates a new user' do
-        expect {
-          post :create, params: valid_params
-          Rails.logger.debug "User errors: #{assigns(:user).errors.full_messages.join(", ")}"
-          
-        }.to change(User, :count).by(1)
-      end
+  after do
+    User.destroy_all  
+    ActionMailer::Base.deliveries.clear  
+  end
 
-      it 'redirects to confirmation_pending_path' do
+  describe 'POST #create' do
+    let(:valid_params) do
+      {
+        user: {
+          name: 'John Doe',
+          lastname: 'Doe',
+          cpf: '12345678900',
+          oab: 'OAB12345',
+          sex: 'Male',
+          birth_date: Date.new(1990, 1, 1),
+          city: 'City',
+          state: 'State',
+          phone: '123456789',
+          email: 'john.doe@example.com',
+          password: 'password',
+          password_confirmation: 'password',
+          terms_of_use: '1'
+        }
+      }
+    end
+
+    it 'creates a new user and sends confirmation email' do
+      expect {
         post :create, params: valid_params
-        expect(response).to redirect_to(pending_confirmation_path)
-      end
+      }.to change(User, :count).by(1)
+
+      user = User.last
+      expect(user.confirmed_at).to be_nil 
+
+      
+      expect(response).to redirect_to(pending_confirmation_path)
+
+      
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+      expect(ActionMailer::Base.deliveries.last.subject).to eq('Confirmation instructions')
+      expect(ActionMailer::Base.deliveries.last.to).to eq([user.email])
     end
   end
 end

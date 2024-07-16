e479e5d96234e82a64a9084ca5b45b3a52ebaf10
Raquel Fonsec <raque-leto@hotmail.com>
2024-07-17 01:28:58 +0200

    removido migraçao confirmable do devise

---
+++ db/migrate/20240716232752_remove_confirmable_from_devise.rb
@@ -0,0 +1,4 @@
+class RemoveConfirmableFromDevise < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_221623) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_232752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_221623) do
     t.string "name"
     t.string "unconfirmed_email"
     t.string "confirmation_token"
+    t.string "rg"
+    t.string "user_type"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
